9f6580c86d80f839a7bac27bd10ef576736f825c
Sam Rapowitz <samrapow@gmail.com>
2022-03-09 23:04:44 -0500

    finished user model

---
+++ models/User.js
@@ -0,0 +1,45 @@
+const { Schema, model, Types } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            match: [/.+@.+\..+/, 'Enter a valid e-mail address']
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            getters: true,
+            virtuals: true
+        }
+    }
+);
+
+UserSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+})
+
+// create user model
+const User = model('User', UserSchema);
+
+module.exports = User;
